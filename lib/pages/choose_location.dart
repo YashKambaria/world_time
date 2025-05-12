cb3cbed3ef599fb8cde996b95872328caa6b86f7
Yash Kambaria <yashkambaria245@gmail.com>
2025-05-12 15:43:05 +0530

    Learned basics of Http and JsonDecode

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -8,33 +8,6 @@ class Location extends StatefulWidget {
 }
 
 class _LocationState extends State<Location> {
-
-  void getdata () async {
-
-    //this will be triggered in future after 3 seconds
-    String username = await Future.delayed(Duration(seconds: 3),(){
-      return "Yash";
-    });
-
-    //this will be triggered in future after 2 seconds
-    String description = await Future.delayed(Duration(seconds:2),(){
-      return "I am a App developer";
-    });
-
-    //now the problem is the name should be printed first and the description afterwards
-    //to solve this we will use async and await
-    print('$username - $description');
-  }
-
-  
-  int counter = 0;
-  @override
-  void initState() {
-    super.initState();
-    print("init build");
-    getdata();
-  }
-
   @override
   Widget build(BuildContext context) {
     print("build only");
@@ -45,14 +18,6 @@ class _LocationState extends State<Location> {
         elevation: 0,
         backgroundColor: Colors.blue[900],
         title: Text("Choose Location", style: TextStyle(color: Colors.white)),
-      ),
-      body: ElevatedButton(
-        onPressed: () {
-          setState(() {
-            counter+=1;
-          });
-        },
-        child: Text("counter is $counter"),
       ),
     );
   }
--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -8,6 +10,17 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+  void getdata () async {
+      Response response= await get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));
+      Map res=jsonDecode(response.body);
+      print(res['title']);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getdata();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
